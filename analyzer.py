482e5567ca306e6f4a6db5c64e7d9e22252ea112
İrem Bağçıvan <230601013@bakircay.edu.tr>
2024-07-04 14:36:00 +0300

    Analysis button interaction

--- analyzer.py
+++ analyzer.py
@@ -1,8 +1,35 @@
 from PyQt5 import QtWidgets
+from collections import Counter
 from interface import Ui_MainWindow
 
 class Analyzer_interface(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.show()
+
+        # button interaction
+        self.pushButton_analysis.clicked.connect(self.analyze_text)
+        self.pushButton_search.clicked.connect(self.search_word)
+    
+    def analyze_text(self):
+        text = self.textEdit.toPlainText()
+        words = text.split() # returns list
+        words_count = len(words)
+
+        if words_count > 0:
+            total_words = words_count
+            self.label_words.setText(f"Total number of words in the text: {total_words}")
+
+            most_repeated_word = Counter(words).most_common(1)[0]
+            self.label_most.setText(f"The most repeated word in the text: {most_repeated_word[0]} ({most_repeated_word[1]} times)")
+
+            least_repeated_word = Counter(words).most_common()[-1]
+            self.label_least.setText(f"The least repeated word in the text: {least_repeated_word[0]} ({least_repeated_word[1]} times)")
+
+        else:
+            self.label_words.setText(f"Total number of words in the text: 0")
+            self.label_least.setText(f"The most repeated word in the text: None")
+            self.label_most.setText(f"The least repeated word in the text: None")
+
+    def search_word(self):
+        pass
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from analyzer import *
+from analyzer import Analyzer_interface
 
 app = QApplication(sys.argv)
 analyzer = Analyzer_interface()
+analyzer.show()
 sys.exit(app.exec_())
